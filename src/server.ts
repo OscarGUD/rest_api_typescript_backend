63ff3febfdadc1394b08f2a595510e9fc4d17fb3
OscarGUD <oscarprogweb@gmail.com>
2026-02-07 21:34:41 -0600

    fix: cors en docs

--- src/server.ts
+++ src/server.ts
@@ -8,10 +8,16 @@ import swaggerSpec, { swaggerUiOptions } from "./config/swagger";
 // Instancia de express
 const server = express();
 
+// Leer datos de formularioo
+server.use(express.json());
+
+// Docs (debe ir ANTES del cors restrictivo)
+server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerUiOptions))
+
 // Conexion cors
 const corsOptions: CorsOptions = {
   origin: function (origin, callback) {
-    if (origin === process.env.FRONTEND_URL) {
+    if (!origin || origin === process.env.FRONTEND_URL) {
       callback(null, true)
     } else {
       callback(new Error('Error de cors'), false)
@@ -20,14 +26,8 @@ const corsOptions: CorsOptions = {
 }
 server.use(cors(corsOptions))
 
-// Leer datos de formularioo
-server.use(express.json());
-
 // rutas
 server.use(morgan('dev'))
 server.use("/api/products", router);
-
-// Docs
-server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerUiOptions))
 
 export default server;
